d2d6fd4010ec2881d544c347d4ecffc42bcd357f
Pann Angel <134255508+pannangel@users.noreply.github.com>
2025-06-24 17:41:44 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+first_num = int(input("Give me the first number: "))
+sec_num = int(input("Give me the second number: "))
+print("Thank you!")
+addition = first_num + sec_num
+print(f"{first_num} + {sec_num} = {addition}")
+subtract = first_num - sec_num
+print(f"{first_num} - {sec_num} = {subtract}")
+division = first_num // sec_num
+print(f"{first_num} / {sec_num} = {division}")
+multiply = first_num * sec_num
+print(f"{first_num} * {sec_num} = {multiply}")
